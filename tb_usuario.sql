bef47ff3e777d92c36daec32e5f883df40a12746
marcos226pimentel <marcos226pimentel@gmail.com>
2022-12-04 10:36:25 -0300

    Commit inicial

---
+++ tb_usuario.sql
@@ -0,0 +1,25 @@
+﻿create schema financas
+
+CREATE TABLE financas.usuario
+(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome character varying(150),
+  email character varying(100),
+  senha character varying(20),
+  data_cadastro date default now()
+);
+
+CREATE TABLE financas.lancamento
+(
+  id bigserial NOT NULL PRIMARY KEY ,
+  descricao character varying(100) NOT NULL,
+  mes integer NOT NULL,
+  ano integer NOT NULL,
+  valor numeric(16,2) NOT NULL,
+  tipo character varying(20) CHECK ( tipo in ('RECEITA', 'DESPESA')) NOT NULL,
+  status character varying(20) CHECK (status IN ('PENDENTE', 'CANCELADO', 'EFETIVADO')) NOT NULL,
+  id_usuario bigint REFERENCES financas.usuario (id) NOT NULL,
+  data_cadastro date default now()
+);
+
+SELECT usename FROM pg_user;
